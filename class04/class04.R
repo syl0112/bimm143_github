5589fd50218ef88142c73ca831b62c9bccdfe5f8
Shuk Yee Leung <syl013@ucsd.edu>
2025-05-20 11:55:43 -0700

    Add class4

---
+++ class04/class04.R
@@ -0,0 +1,13 @@
+#' ---
+#' title: "Intro to R"
+#' author: "Shuk Yee Leung"
+#' date: "April 9th, 2024"
+#' ---
+
+# My first R script
+x <- 1:50
+plot(x, sin(x))
+
+plot(x, sin(x), typ="l", col="green", lwd=3, xlab="Silly x axis", ylab="Sensible y axis") 
+
+plot (cdc$height)
